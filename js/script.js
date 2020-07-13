eb8bf802caff020b3aea149ecb071d1d21a45dd7
argreality <65363004+argreality@users.noreply.github.com>
2020-07-13 22:03:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ const colors = [
     //usdz: 'android/beds/glbs/PINK2.usdz',
 },
 {
-    texture: 'resources/cream.png',
+    texture: 'resources/cream.jpg',
     size: [1,1,1],
     shininess: 0,
     link: 'android/beds/4.html',
